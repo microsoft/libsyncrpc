3849ddc3cbb21703d280bd0cb1f61ec6b3e21d28
Kat Marchán <kzm@zkat.tech>
2024-10-15 18:33:25 -0700

    refactor things and implement an mmap-based synchronizer

--- src/lib.rs
+++ src/lib.rs
@@ -2,43 +2,46 @@ use std::{
   collections::HashMap,
   io::{BufRead, BufReader, BufWriter, Lines, Write as _},
   process::{Child, ChildStdin, ChildStdout},
+  time::Duration,
 };
 
+use bytecheck::CheckBytes;
+use mmap_sync::synchronizer::Synchronizer;
 use napi::{
   bindgen_prelude::{Function, Result},
   Error,
 };
+use rkyv::{Archive, Deserialize, Serialize};
+use tempfile::TempDir;
 
 #[macro_use]
 extern crate napi_derive;
 
 /// A synchronous RPC channel that allows JavaScript to synchronously call out
-/// to a child process and get a response over a line-based protocol,
+/// to a child process and get a response over a simple tab-delimited protocol,
 /// including handling of JavaScript-side callbacks before the call completes.
 /// For details on the protocol, see the `requestSync` method.
 #[napi]
 pub struct SyncRpcChannel {
-  child: Child,
-  lines: Lines<BufReader<ChildStdout>>,
-  writer: BufWriter<ChildStdin>,
+  ipc: Box<dyn IPCHandler>,
   callbacks: HashMap<String, Function<'static, (String, String), String>>,
 }
 
 #[napi]
 impl SyncRpcChannel {
-  #[napi(constructor)]
-  pub fn new(exe: String, args: Vec<String>) -> Result<Self> {
-    let mut child = std::process::Command::new(exe)
-      .stdin(std::process::Stdio::piped())
-      .stdout(std::process::Stdio::piped())
-      .args(args)
-      .spawn()?;
-
+  #[napi(factory)]
+  pub fn with_line_protocol(exe: String, args: Vec<String>) -> Result<Self> {
     Ok(Self {
-      lines: BufReader::new(child.stdout.take().unwrap()).lines(),
-      writer: BufWriter::new(child.stdin.take().unwrap()),
+      ipc: Box::new(SocketLineIPC::new(exe, args)?),
       callbacks: HashMap::new(),
-      child,
+    })
+  }
+
+  #[napi(factory)]
+  pub fn with_mmap_protocol(exe: String, args: Vec<String>) -> Result<Self> {
+    Ok(Self {
+      ipc: Box::new(MmapIPC::new(exe, args)?),
+      callbacks: HashMap::new(),
     })
   }
 
@@ -89,13 +92,8 @@ impl SyncRpcChannel {
   ///   `call-response` and `call-error` messages.
   #[napi]
   pub fn request_sync(&mut self, method: String, payload: String) -> Result<String> {
-    self.writer.write_all(b"request\t")?;
-    self.writer.write_all(method.as_bytes())?;
-    self.writer.write_all(b"\t")?;
-    self.writer.write_all(payload.as_bytes())?;
-    self.writer.write_all(b"\n")?;
-    self.writer.flush()?;
-    for line in &mut self.lines {
+    self.ipc.write_message("request", &method, &payload)?;
+    while let Some(line) = self.ipc.read_message() {
       let line = line?;
       let mut parts = line.splitn(3, '\t');
       let (ty, name, payload) = (
@@ -117,55 +115,47 @@ impl SyncRpcChannel {
           if name == method {
             return Ok(payload.to_string());
           } else {
-            return Err(Error::from_reason(
-              format!("name mismatch for response: expected `{method}`, got `{name}`"),
-            ));
+            return Err(Error::from_reason(format!(
+              "name mismatch for response: expected `{method}`, got `{name}`"
+            )));
           }
         }
         "error" => {
           if name == method {
             return Err(Error::from_reason(payload));
           } else {
-            return Err(Error::from_reason(
-              format!("name mismatch for response: expected `{method}`, got `{name}`"),
-            ));
+            return Err(Error::from_reason(format!(
+              "name mismatch for response: expected `{method}`, got `{name}`"
+            )));
           }
         }
         "call" => {
           if let Some(cb) = self.callbacks.get(name) {
             match cb.call((name.into(), payload.into())) {
               Ok(res) => {
-                self.writer.write_all(b"call-response\t")?;
-                self.writer.write_all(name.as_bytes())?;
-                self.writer.write_all(b"\t")?;
-                self.writer.write_all(res.trim().as_bytes())?;
-                self.writer.write_all(b"\n")?;
-                self.writer.flush()?;
+                self.ipc.write_message("call-response", name, res.trim())?;
               }
               Err(e) => {
-                self.writer.write_all(b"call-error\t")?;
-                self.writer.write_all(name.as_bytes())?;
-                self.writer.write_all(b"\t")?;
-                self.writer.write_all(format!("{e}").trim().as_bytes())?;
-                self.writer.write_all(b"\n")?;
-                self.writer.flush()?;
-                return Err(Error::from_reason(format!("Error calling callback `{name}`: {}", e)));
+                self
+                  .ipc
+                  .write_message("call-error", name, format!("{e}").trim())?;
+                return Err(Error::from_reason(format!(
+                  "Error calling callback `{name}`: {}",
+                  e
+                )));
               }
             }
           } else {
-            self.writer.write_all(b"call-error\t")?;
-            self.writer.write_all(name.as_bytes())?;
-            self.writer.write_all(b"\t")?;
-            self.writer.write_all(format!("unknown callback: `{name}`. Please make sure to register it on the JavaScript side before invoking it.").trim().as_bytes())?;
-            self.writer.write_all(b"\n")?;
-            self.writer.flush()?;
-            return Err(Error::from_reason(
-              format!("no callback named `{name}` found"),
-            ));
+            self.ipc.write_message("call-error", name, &format!("unknown callback: `{name}`. Please make sure to register it on the JavaScript side before invoking it."))?;
+            return Err(Error::from_reason(format!(
+              "no callback named `{name}` found"
+            )));
           }
         }
         _ => {
-          return Err(Error::from_reason(format!("Invalid message type from child: `{ty}`")));
+          return Err(Error::from_reason(format!(
+            "Invalid message type from child: `{ty}`"
+          )));
         }
       }
     }
@@ -180,7 +170,11 @@ impl SyncRpcChannel {
   /// If the callback throws, an it will be handled appropriately by
   /// `requestSync` and the child will be notified.
   #[napi]
-  pub fn register_callback(&mut self, name: String, cb: Function<'static, (String, String), String>) {
+  pub fn register_callback(
+    &mut self,
+    name: String,
+    cb: Function<'static, (String, String), String>,
+  ) {
     self.callbacks.insert(name, cb);
   }
 
@@ -188,6 +182,112 @@ impl SyncRpcChannel {
   /// would you? Just what kind of person are you?
   #[napi]
   pub fn murder_in_cold_blood(&mut self) -> Result<()> {
+    self.ipc.close()?;
+    Ok(())
+  }
+}
+
+struct SocketLineIPC {
+  child: Child,
+  lines: Lines<BufReader<ChildStdout>>,
+  writer: BufWriter<ChildStdin>,
+}
+
+impl SocketLineIPC {
+  fn new(exe: String, args: Vec<String>) -> Result<Self> {
+    let mut child = std::process::Command::new(exe)
+      .stdin(std::process::Stdio::piped())
+      .stdout(std::process::Stdio::piped())
+      .args(args)
+      .spawn()?;
+
+    Ok(Self {
+      lines: BufReader::new(child.stdout.take().unwrap()).lines(),
+      writer: BufWriter::new(child.stdin.take().unwrap()),
+      child,
+    })
+  }
+}
+
+trait IPCHandler {
+  fn read_message(&mut self) -> Option<Result<String>>;
+  fn write_message(&mut self, ty: &str, name: &str, payload: &str) -> Result<()>;
+  fn close(&mut self) -> Result<()>;
+}
+
+impl IPCHandler for SocketLineIPC {
+  fn read_message(&mut self) -> Option<Result<String>> {
+    self.lines.next().map(|line| Ok(line?))
+  }
+
+  fn write_message(&mut self, ty: &str, name: &str, payload: &str) -> Result<()> {
+    self.writer.write_all(ty.as_bytes())?;
+    self.writer.write_all(b"\t")?;
+    self.writer.write_all(name.as_bytes())?;
+    self.writer.write_all(b"\t")?;
+    self.writer.write_all(payload.as_bytes())?;
+    self.writer.write_all(b"\n")?;
+    self.writer.flush()?;
+    Ok(())
+  }
+
+  fn close(&mut self) -> Result<()> {
+    self.child.kill()?;
+    Ok(())
+  }
+}
+
+struct MmapIPC {
+  child: Child,
+  reader_tmp: TempDir,
+  reader: Synchronizer,
+  writer_tmp: TempDir,
+  writer: Synchronizer,
+}
+
+impl MmapIPC {
+  fn new(exe: String, args: Vec<String>) -> Result<Self> {
+    let reader_tmp = TempDir::new()?;
+    let writer_tmp = TempDir::new()?;
+    let child = std::process::Command::new(exe).args(args).spawn()?;
+
+    Ok(Self {
+      child,
+      reader: Synchronizer::new(reader_tmp.path().join("reader").as_os_str()),
+      writer: Synchronizer::new(writer_tmp.path().join("writer").as_os_str()),
+      reader_tmp,
+      writer_tmp,
+    })
+  }
+}
+
+/// Example data-structure shared between writer and reader(s)
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[archive_attr(derive(CheckBytes))]
+struct MmapMsg(String);
+
+impl IPCHandler for MmapIPC {
+  fn read_message(&mut self) -> Option<Result<String>> {
+    let line = unsafe {
+      self
+        .reader
+        .read::<MmapMsg>(true)
+        .map_err(|e| Error::from_reason(format!("{e}")))
+    }
+    .map(|line| format!("{}", line.0));
+    Some(line)
+  }
+
+  fn write_message(&mut self, ty: &str, name: &str, payload: &str) -> Result<()> {
+    let data = MmapMsg(format!("{}\t{}\t{}", ty, name, payload));
+    self
+      .writer
+      .write(&data, Duration::from_secs(1))
+      .map_err(|e| Error::from_reason(format!("Failed to send mmap message: {e}")))?;
+    Ok(())
+  }
+
+  fn close(&mut self) -> Result<()> {
     self.child.kill()?;
     Ok(())
   }
