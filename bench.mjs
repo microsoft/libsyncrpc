9fed9c0ba3b27283955bb88f097bf5ff6ca30294
Kat Marchán <kzm@zkat.tech>
2025-03-11 16:48:34 -0700

    stuff

--- bench.mjs
+++ bench.mjs
@@ -2,18 +2,24 @@ import { Bench } from 'tinybench';
 import { SyncRpcChannel } from "./index.js";
 
 const rustChannel = new SyncRpcChannel("cargo", ["run", "--release", "--example", "socket_child"])
-const nodeChannel = new SyncRpcChannel("node", ["./echo.mjs"])
+// const nodeChannel = new SyncRpcChannel("node", ["./echo.mjs"])
 const bench = new Bench();
+
+const smallMsg = '"hello"';
+const bigMsg = 'x'.repeat(10 * 1024 * 1024);
 
 bench
     // .add("baseline function call", () => {
     //     exampleFun("echo", '"hello"');
     // })
-    .add('simple echo request to Node child', () => {
-        nodeChannel.requestSync("echo", '"hello"');
-    })
+    // .add('simple echo request to Node child', () => {
+    //     nodeChannel.requestSync("echo", '"hello"');
+    // })
     .add('simple echo request to Rust child', () => {
-        rustChannel.requestSync("echo", '"hello"');
+        rustChannel.requestSync("echo", smallMsg);
+    })
+    .add('simple echo request to Rust child with a bigger 1MiB message', () => {
+        rustChannel.requestSync("echo", bigMsg);
     });
 
 await bench.warmup(); // make results more reliable, ref: https://github.com/tinylibs/tinybench/pull/50
